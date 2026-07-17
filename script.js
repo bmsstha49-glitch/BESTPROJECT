e86344893008c9dbb43d3eb9b62fc333a9b2f845
bmsstha49-glitch <bmsstha49@gmail.com>
2026-07-17 08:36:34 +0545

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,38 @@ function startJourney(){
 
 
 
+let letterIndex = 0;
+
+
+const birthdayMessage = `
+
+Happy birthday, my girl ❤️
+
+This is such a special day and I hope this little message makes your day a little better.
+
+May the seas run dry.
+May the winds stop blowing.
+
+My mind will always revolve around one person,
+and that's you, beautiful girl.
+
+Happiest birthday to you, my sweetheart,
+my charm, and my peace of mind.
+
+I hope everything goes well for you,
+and I hope you always stay happy.
+
+I miss you so much 🥹
+
+Let's make beautiful memories together.
+
+❤️
+
+`;
+
+
+
+
 function openLetter(){
 
     document
@@ -21,6 +53,46 @@ function openLetter(){
     .add("hidden");
 
 
-    alert("Letter scene coming next ❤️");
+    document
+    .getElementById("letterScene")
+    .classList
+    .remove("hidden");
+
+
+    typeLetter();
+
+}
+
+
+
+
+
+function typeLetter(){
+
+    let text =
+    document.getElementById("letterText");
+
+
+    if(letterIndex < birthdayMessage.length){
+
+        text.innerHTML += 
+        birthdayMessage.charAt(letterIndex);
+
+
+        letterIndex++;
+
+
+        setTimeout(typeLetter,45);
+
+    }
+
+    else{
+
+        document
+        .getElementById("continueBtn")
+        .classList
+        .remove("hidden");
+
+    }
 
 }
